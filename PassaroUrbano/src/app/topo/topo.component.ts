c407c13dda67d95be6a67a84b142bc1b56a31cf9
Leonardo Maturano <leomaturano@gmail.com>
2017-10-04 20:24:17 -0300

    Configurações iniciais e copia de arquivos

---
+++ PassaroUrbano/src/app/topo/topo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'urbano-topo',
+  templateUrl: './topo.component.html',
+  styleUrls: ['./topo.component.css']
+})
+export class TopoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
